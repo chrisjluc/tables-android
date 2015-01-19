f21181a06ab44914717c4fcfaa69be8b6a4677d2
Christopher Luc <chris.luc.dev@gmail.com>
2015-01-18 17:12:02 -0800

    Added bitmap caching and more efficient loading Ref:http://developer.android.com/training/displaying-bitmaps/index.html

--- android/app/src/main/java/tables/android/TablesApp.java
+++ android/app/src/main/java/tables/android/TablesApp.java
@@ -4,11 +4,14 @@ import android.app.Application;
 
 import com.parse.Parse;
 
+import tables.android.framework.BitmapManager;
+
 public class TablesApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         Parse.enableLocalDatastore(this);
         Parse.initialize(this, "nRy0P11TV7YcSE86h0yyXlQW6bkIWSRZeOtnGEOp", "nNSzr2MFlNIoydrFdWrV1uYl3BReFWyOZNvWiqZn");
+        BitmapManager.init(getApplicationContext());
     }
 }
--- android/app/src/main/java/tables/android/adapters/RestaurantsAdapter.java
+++ android/app/src/main/java/tables/android/adapters/RestaurantsAdapter.java
@@ -1,59 +1,62 @@
-package tables.android.ui.adapters;
+package tables.android.adapters;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import tables.android.R;
+import tables.android.framework.BitmapManager;
 import tables.android.models.Restaurant;
-import tables.android.ui.FindRestaurantsActivity;
 
 public class RestaurantsAdapter extends RecyclerView.Adapter<RestaurantsAdapter.ViewHolder> {
+    private final static String TAG = "RestaurantsAdapter";
     private Restaurant[] mRestaurants;
     private Activity mActivity;
+    private BitmapManager mBitmapManager;
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
         public CardView mCardView;
+
         public ViewHolder(CardView v) {
             super(v);
             mCardView = v;
         }
     }
 
-    // Provide a suitable constructor (depends on the kind of dataset)
     public RestaurantsAdapter(Restaurant[] restaurants, Activity activity) {
         mRestaurants = restaurants;
         mActivity = activity;
+        mBitmapManager = BitmapManager.getInstance();
     }
 
-    // Create new views (invoked by the layout manager)
     @Override
     public RestaurantsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
-                                                   int viewType) {
-        // create a new view
+                                                            int viewType) {
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.card_view_restaurant, parent, false);
-        // set the view's size, margins, paddings and layout parameters
         ViewHolder vh = new ViewHolder((CardView) v);
         return vh;
     }
 
-    // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(ViewHolder holder, final int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
-        TextView restaurantNameTextView = (TextView) holder.mCardView.findViewById(R.id.restaurant_name_text_view);
-        restaurantNameTextView.setText(mRestaurants[position].getRestaurantName());
+        Restaurant r = mRestaurants[position];
+        TextView restaurantNameTextView = (TextView) holder.mCardView.findViewById(R.id.restaurantNameTextView);
+        if (r.getRestaurantName() != null)
+            restaurantNameTextView.setText(r.getRestaurantName());
+        ImageView restaurantCoverPhotoImageView = (ImageView) holder.mCardView.findViewById(R.id.restaurantCoverPhotoImageView);
+        if (r.getRestaurantCoverPhotoLink() != null) {
+            mBitmapManager.loadBitmap(r.getRestaurantCoverPhotoLink(), restaurantCoverPhotoImageView);
+        }
+        TextView restaurantTypeTextView = (TextView) holder.mCardView.findViewById(R.id.restaurantTypeTextView);
+        if (r.getRestaurantType() != null) {
+            restaurantTypeTextView.setText(r.getRestaurantType());
+        }
         holder.mCardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -61,10 +64,8 @@ public class RestaurantsAdapter extends RecyclerView.Adapter<RestaurantsAdapter.
 //                mActivity.startActivity(intent);
             }
         });
-
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
         return mRestaurants.length;
